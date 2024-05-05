4c2afcb2b37b4abacecde179de27b63f615626b7
UchaBokeria <ucha1bokeria@gmail.com>
2024-05-05 06:09:54 +0400

    fx

--- server/controller/branches/branches.controller.go
+++ server/controller/branches/branches.controller.go
@@ -1,8 +1,6 @@
 package branches
 
 import (
-	"log"
-
 	"main/build/view"
 	"main/server/common/controller"
 	"main/server/common/storage"
@@ -13,8 +11,5 @@ func index(ctx *controller.Context) error {
 	var Branches []model.Branches
 	storage.DB.Preload("City").Preload("Shifts").Find(&Branches)
 
-	for _, v := range Branches {
-		log.Printf(" a %T \n", v)
-	}
 	return ctx.Html(view.Branches(Branches))
 }
